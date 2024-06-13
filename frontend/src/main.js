5f0c4104468ab9d03e555f0972c61365c540fd13
PaolaLu <paolaluciasepulveda@gmail.com>
2024-06-13 08:16:35 -0300

    vuetify y axios instalados

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import VueAxios from "vue-axios";
+import custom_axios from "./plugins/axios";
+
 
 Vue.config.productionTip = false
 
+Vue.use(VueAxios, custom_axios);
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
---
+++ frontend/src/plugins/axios.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const custom_axios = axios.create(
+    {
+        baseURL: 'http://127.0.0.1:8000',
+    }
+)
+export default custom_axios;
